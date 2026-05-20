c7771fb57fae0833ee6b1513da549ec970c0a7ce
CBSebastian-Flores <brunosebastianflores@gmail.com>
2026-05-20 18:41:56 -0500

    resolviendo conflictos 3

--- backend/src/main/java/com/arenales/config/JwtFilter.java
+++ backend/src/main/java/com/arenales/config/JwtFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
--- backend/src/main/java/com/arenales/controllers/AuthController.java
+++ backend/src/main/java/com/arenales/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.arenales.controllers;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
