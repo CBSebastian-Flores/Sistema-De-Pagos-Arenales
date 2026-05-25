983a2bba592661dd086113d1414ccc9ea0b37b62
CBSebastian-Flores <brunosebastianflores@gmail.com>
2026-05-25 18:10:43 -0500

    feat(backend): configurar cliente HTTP para consumir API de DNI (SDPA-49)

---
+++ backend/src/main/java/com/arenales/services/ReniecService.java
@@ -0,0 +1,8 @@
+package com.arenales.services;
+
+import com.arenales.dto.UsuarioDTO;
+import com.arenales.entities.Usuario;
+
+public interface ReniecService {
+    boolean existeDni(String dni);
+}
