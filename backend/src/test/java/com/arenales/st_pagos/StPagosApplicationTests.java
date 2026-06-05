4722dbff98fc0392179da2427804a42d48d5207b
Yo5h1 <joaquinskt04@gmail.com>
2026-06-05 16:24:38 -0500

    SDPA-69: UI tabla mantenimiento de usuarios con modales de edicion y restablecimiento de contrasena

--- backend/src/test/java/com/arenales/st_pagos/StPagosApplicationTests.java
+++ backend/src/test/java/com/arenales/st_pagos/StPagosApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StPagosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
