d185e7bc4ceb0f1914415441e763cf94f3f588f1
CBSebastian-Flores <brunosebastianflores@gmail.com>
2026-06-06 19:01:39 -0500

    Terminando los dtos

---
+++ backend/src/main/java/com/arenales/dto/ServicioListadoDTO.java
@@ -0,0 +1,18 @@
+package com.arenales.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServicioListadoDTO {
+    private Integer idServicio;
+    private String nombreServicio;
+    private String descripcion;
+    private BigDecimal precioBase;
+    private Boolean estado;
+}
