ad1871a823115fc98dfec55580542d9e03692dd0
Franco Velasquez <velasquezfranco07@gmail.com>
2026-07-15 12:01:40 -0500

    SDPA-159: Se desarrollo el SDPA-175 - Endpoint de lectura del historial  de servicios completado

---
+++ backend/src/main/java/com/arenales/controllers/HistorialServicioController.java
@@ -0,0 +1,28 @@
+package com.arenales.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.arenales.dto.HistorialServicioResponseDTO;
+import com.arenales.services.HistorialServicioService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/historial-servicio")
+public class HistorialServicioController {
+
+    @Autowired
+    private HistorialServicioService historialServicioService;
+
+    @GetMapping("/listar")
+    @PreAuthorize("hasAnyAuthority('Administrador')")
+    public ResponseEntity<List<HistorialServicioResponseDTO>> obtenerHistorialServicios() {
+        List<HistorialServicioResponseDTO> historial = historialServicioService.obtenerHistorialCompleto();
+        return ResponseEntity.ok(historial);
+    }
+
+}
---
+++ backend/src/main/java/com/arenales/dto/HistorialServicioResponseDTO.java
@@ -0,0 +1,14 @@
+package com.arenales.dto;
+import lombok.Data;
+
+@Data
+public class HistorialServicioResponseDTO {
+
+    private Integer idHistorialServicio;
+    private Integer idServicio;
+    private String nombreServicio;
+    private String datosAnteriores;
+    private String tipoAccion;
+    private String motivo;
+    private String nombreUsuarioCreador;
+}
